d1080e371baeea9b2fccb90e3996cc9b9fcf7035
Tibor <tibor736@hotmail.com>
2023-07-09 02:13:31 +0300

    05: added read more button and refresh list

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,26 @@ const App = () => {
 		);
 	}
 
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className="title">
+					<h2>no tours left</h2>
+					<button
+						type="button"
+						className="btn"
+						onClick={() => fetchTours()}
+					>
+						refresh
+					</button>
+				</div>
+			</main>
+		);
+	}
+
 	return (
 		<main>
-			<Tours tours={tours} removeTour={removeTour}/>
+			<Tours tours={tours} removeTour={removeTour} />
 		</main>
 	);
 };
